9ae6ed2a0d3d9e86b584c831e18cfc330fa287d3
Fitz Elliott <felliott@fiskur.org>
2014-01-16 13:18:44 -0500

    initial commit

---
+++ lib/Task/BeLike/FELLIOTT.pm
@@ -0,0 +1,39 @@
+package Task::BeLike::FELLIOTT;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::BeLike::FELLIOTT - It's new $module
+
+=head1 SYNOPSIS
+
+    use Task::BeLike::FELLIOTT;
+
+=head1 DESCRIPTION
+
+Task::BeLike::FELLIOTT is ...
+
+=head1 LICENSE
+
+Copyright (C) Fitz Elliott.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Fitz Elliott E<lt>felliott@fiskur.orgE<gt>
+
+=cut
+
---
+++ t/00_compile.t
@@ -0,0 +1,9 @@
+use strict;
+use Test::More;
+
+use_ok $_ for qw(
+    Task::BeLike::FELLIOTT
+);
+
+done_testing;
+
